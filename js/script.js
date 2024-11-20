248b302351b9cd5fe2d2bb63f8304b0696e77c4f
Sahara Islam <am.saharaislam@gmail.com>
2024-11-21 00:32:38 +0600

    ui upload done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-$(document).ready(function () {
-    // Initialize Masonry.js
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-item',
-        percentPosition: true,
-        gutter: 10
-    });
-});
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// init Masonry
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });  
+  
+
+  Fancybox.bind('[data-fancybox]', {
+    // Your custom options
+  }); 
